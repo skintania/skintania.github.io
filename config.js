3138502f3831b79e111c20324c69ff18643db56e
BeamSuvijak <beamsvj@gmail.com>
2026-03-23 18:28:42 +0700

    config help

--- config.js
+++ config.js
@@ -4,3 +4,12 @@ const CONFIG = {
 // วิธีใช้ import config.js เข้ามา
 // จะมีตัวแปร API_URL
 // วิธีเรียก api_url/bucket_name?file=path_to_file
+
+/*
+อย่าลืม pass header เข้าไปด้วย
+fetch("https://your-worker.workers.dev/skdrive", {
+  headers: {
+    "Authorization": "Bearer " + localStorage.getItem("user_token")
+  }
+})
+*/
